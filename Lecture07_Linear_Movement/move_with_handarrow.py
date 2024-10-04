fd4df3e8fe2cf92af5fde457b7456616287e52a5
seongjun-jin <wlstjdwns2108@naver.com>
2024-10-04 18:54:49 +0900

    esc,x클릭시 종료 구현

--- Lecture07_Linear_Movement/move_with_handarrow.py
+++ Lecture07_Linear_Movement/move_with_handarrow.py
@@ -2,12 +2,22 @@ from pico2d import *
 import random
 
 
-TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+TUK_WIDTH, TUK_HEIGHT = 800, 600
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 hand_arrow = load_image('hand_arrow.png')
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
 
 def random_hand_arrow():
     h_x = random.randrange(0, 800)
@@ -18,7 +28,11 @@ def move_with_hand_arrow():
 
     pass
 
-while(True):
+
+running = True
+
+while running:
     random_hand_arrow()
-    move_with_hand_arrow()
+    #move_with_hand_arrow()
+    handle_events()
 
